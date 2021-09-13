9674b367847e26edff225eb03d8c75629068917c
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2021-09-13 13:51:29 +0200

    Upgrade to 1.4
    * Upgrade Central Dashboard image to 1.4rc0
    * Change GH Actions tests to pytest operator
    * Add charmcraft parts

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from subprocess import check_output
 from time import sleep
 
@@ -7,6 +8,34 @@ from selenium import webdriver
 from selenium.common.exceptions import JavascriptException, WebDriverException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test):
+    my_charm = await ops_test.build_charm(".")
+    image_path = METADATA["resources"]["oci-image"]["upstream-source"]
+
+    await ops_test.model.deploy(my_charm, resources={"oci-image": image_path})
+    await ops_test.model.deploy("cs:kubeflow-profiles")
+    await ops_test.model.add_relation("kubeflow-profiles", "kubeflow-dashboard")
+
+    await ops_test.model.block_until(
+        lambda: all(
+            unit.workload_status == "active" and unit.agent_status == "idle"
+            for _, application in ops_test.model.applications.items()
+            for unit in application.units
+        ),
+        timeout=600,
+    )
+
+    await ops_test.model.wait_for_idle(raise_on_error=False, wait_for_active=True)
+
+
+async def test_status(ops_test):
+    charm_name = METADATA["name"]
+    assert ops_test.model.applications[charm_name].units[0].workload_status == "active"
 
 
 def fix_queryselector(elems):
@@ -25,10 +54,17 @@ def fix_queryselector(elems):
 
 
 @pytest.fixture()
-def driver(request):
-    status = yaml.safe_load(check_output(["juju", "status", "--format=yaml"]))
+async def driver(request, ops_test):
+    status = yaml.safe_load(
+        check_output(
+            ["juju", "status", "-m", ops_test.model_full_name, "--format=yaml"]
+        )
+    )
     endpoint = status["applications"]["kubeflow-dashboard"]["address"]
-    url = f"http://{endpoint}.nip.io:8082/"
+    application = ops_test.model.applications["kubeflow-dashboard"]
+    config = await application.get_config()
+    port = config["port"]["value"]
+    url = f"http://{endpoint}.nip.io:{port}/"
     options = Options()
     options.headless = True
 
