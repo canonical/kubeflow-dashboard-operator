5afbc1ca99992d8462d5fd3df41742de83a13530
misohu <michal.hucko@canonical.com>
2022-08-04 15:16:21 +0200

    feat: add unit tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -21,7 +21,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from pytest_operator.plugin import OpsTest
 
 
+BASE_LINKS = json.loads(Path("./src/config/sidebar_config.json").read_text())
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+CHARM_NAME = METADATA["name"]
+TENSORBOARD_CHARM_NAME = "tensorboards-web-app"
+PROFILES_CHARM_NAME = "kubeflow-profiles"
 
 
 @pytest.fixture()
@@ -34,8 +38,8 @@ async def driver(ops_test: OpsTest) -> Tuple[webdriver.Chrome, WebDriverWait, st
         "--format=yaml",
     )
     status = yaml.safe_load(tmp[1])
-    address = status["applications"]["kubeflow-dashboard"]["address"]
-    config = await ops_test.model.applications["kubeflow-dashboard"].get_config()
+    address = status["applications"][CHARM_NAME]["address"]
+    config = await ops_test.model.applications[CHARM_NAME].get_config()
     port = config["port"]["value"]
     url = f"http://{address}.nip.io:{port}/"
     options = Options()
@@ -52,9 +56,7 @@ async def driver(ops_test: OpsTest) -> Tuple[webdriver.Chrome, WebDriverWait, st
                 sleep(5)
         else:
             driver.get(url)
-
         yield driver, wait, url
-
         driver.get_screenshot_as_file("/tmp/selenium-dashboard.png")
 
 
@@ -80,27 +82,25 @@ async def test_build_and_deploy(ops_test: OpsTest):
         my_charm, resources={"oci-image": image_path}, trust=True
     )
 
-    charm_name = METADATA["name"]
     await ops_test.model.wait_for_idle(
-        [charm_name],
+        [CHARM_NAME],
         raise_on_blocked=True,
         raise_on_error=True,
         timeout=300,
     )
-    assert ops_test.model.applications[charm_name].units[0].workload_status == "waiting"
+    assert ops_test.model.applications[CHARM_NAME].units[0].workload_status == "waiting"
     assert (
-        ops_test.model.applications[charm_name].units[0].workload_status_message
+        ops_test.model.applications[CHARM_NAME].units[0].workload_status_message
         == "Waiting for kubeflow-profiles relation data"
     )
 
 
 @pytest.mark.abort_on_fail
 async def test_add_profile_relation(ops_test: OpsTest):
-    charm_name = METADATA["name"]
-    await ops_test.model.deploy("kubeflow-profiles", channel="latest/edge", trust=True)
-    await ops_test.model.add_relation("kubeflow-profiles", charm_name)
+    await ops_test.model.deploy(PROFILES_CHARM_NAME, channel="latest/edge", trust=True)
+    await ops_test.model.add_relation(PROFILES_CHARM_NAME, CHARM_NAME)
     await ops_test.model.wait_for_idle(
-        ["kubeflow-profiles", charm_name],
+        [PROFILES_CHARM_NAME, CHARM_NAME],
         status="active",
         raise_on_blocked=True,
         raise_on_error=True,
@@ -109,8 +109,7 @@ async def test_add_profile_relation(ops_test: OpsTest):
 
 
 async def test_status(ops_test: OpsTest):
-    charm_name = METADATA["name"]
-    assert ops_test.model.applications[charm_name].units[0].workload_status == "active"
+    assert ops_test.model.applications[CHARM_NAME].units[0].workload_status == "active"
 
 
 async def test_configmap_exist():
@@ -188,8 +187,9 @@ async def test_default_sidebar_links_missing_tensoboards(
 
 
 async def test_configmap_contents(ops_test: OpsTest):
-    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    config = await ops_test.model.applications[CHARM_NAME].get_config()
+    expected_links = BASE_LINKS
+    configmap = Client().get(ConfigMap, config["dashboard-configmap"]["value"])
     links = json.loads(configmap.data["links"])
     assert links == expected_links
 
@@ -197,19 +197,20 @@ async def test_configmap_contents(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_add_sidebar_tensorboard_relation(ops_test: OpsTest):
     # Change to concrete charm after publish ("tensorboard")
+    # COMMENT START
     charm_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/tensorboards-web-app_ubuntu-20.04-amd64.charm"
     metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
     metadata = yaml.safe_load(Path(metadata_path).read_text())
     image_path = metadata["resources"]["oci-image"]["upstream-source"]
-
     await ops_test.model.deploy(charm_path, resources={"oci-image": image_path})
-    tensorboard_charm_name = metadata["name"]
-    charm_name = METADATA["name"]
+    # COMMENT END use this
+    # await ops_test.model.deploy(TENSORBOARD_CHARM_NAME, channel="latest/edge", trust=True)
+
     await ops_test.model.add_relation(
-        f"{tensorboard_charm_name}:sidebar", f"{charm_name}:sidebar"
+        f"{TENSORBOARD_CHARM_NAME}:sidebar", f"{CHARM_NAME}:sidebar"
     )
     await ops_test.model.wait_for_idle(
-        [tensorboard_charm_name],
+        [TENSORBOARD_CHARM_NAME, CHARM_NAME],
         raise_on_blocked=True,
         raise_on_error=True,
         timeout=300,
@@ -217,19 +218,18 @@ async def test_add_sidebar_tensorboard_relation(ops_test: OpsTest):
 
 
 async def test_configmap_link_added_on_new_sidebar_relation(ops_test: OpsTest):
-    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
-    metadata = yaml.safe_load(Path(metadata_path).read_text())
-    tensorboard_charm_name = metadata["name"]
+    config = await ops_test.model.applications[CHARM_NAME].get_config()
     tensorboard_link = {
-        "app": tensorboard_charm_name,
+        "app": TENSORBOARD_CHARM_NAME,
         "type": "item",
         "link": "/tensorboards/",
         "text": "Tensorboards",
         "icon": "assessment",
     }
-    base_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
+    base_links = BASE_LINKS
     base_links["menuLinks"] = base_links["menuLinks"] + [tensorboard_link]
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    sleep(10)  # sometimes it takes a while for the configmap to be updated
+    configmap = Client().get(ConfigMap, config["dashboard-configmap"]["value"])
     links = json.loads(configmap.data["links"])
     assert links == base_links
 
@@ -260,25 +260,23 @@ async def test_tensorboard_added_sidebar_links(
 async def test_configmap_link_removed_on_removed_sidebar_relation(
     ops_test: OpsTest, driver: Tuple[webdriver.Chrome, WebDriverWait, str]
 ):
-    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
-    metadata = yaml.safe_load(Path(metadata_path).read_text())
-    tensorboard_charm_name = metadata["name"]
-    charm_name = METADATA["name"]
-    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
+    config = await ops_test.model.applications[CHARM_NAME].get_config()
+    expected_links = BASE_LINKS
     await ops_test.run(
         "juju",
         "remove-relation",
-        f"{tensorboard_charm_name}:sidebar",
-        f"{charm_name}:sidebar",
+        f"{TENSORBOARD_CHARM_NAME}:sidebar",
+        f"{CHARM_NAME}:sidebar",
     )
     await ops_test.model.wait_for_idle(
-        [charm_name],
+        [TENSORBOARD_CHARM_NAME, CHARM_NAME],
         status="active",
         raise_on_blocked=True,
         raise_on_error=True,
         timeout=300,
     )
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    sleep(10)  # sometimes it takes a while for the configmap to be updated
+    configmap = Client().get(ConfigMap, config["dashboard-configmap"]["value"])
     links = json.loads(configmap.data["links"])
     assert links == expected_links
     driver, wait, url = await driver.__anext__()
--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -3,39 +3,114 @@
 from unittest.mock import patch
 
 import pytest
-from ops.model import BlockedStatus, WaitingStatus
+import yaml
+
+from ops.model import BlockedStatus, WaitingStatus, ActiveStatus
 from ops.testing import Harness
+from pathlib import Path
 
 from charm import KubeflowDashboardOperator
 
 
-@pytest.fixture
-def harness():
+BASE_SIDEBAR = Path("src/config/sidebar_config.json").read_text()
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+CHARM_NAME = METADATA["name"]
+
+
+@pytest.fixture(scope="function")
+def harness() -> Harness:
     harness = Harness(KubeflowDashboardOperator)
     # Remove when this bug is resolved: https://github.com/kubeflow/kubeflow/issues/6136
     harness.set_model_name("kubeflow")
+    yield harness
+    harness.cleanup()
+
+
+@pytest.fixture(scope="function")
+def harness_with_relation(harness: Harness) -> Harness:
+    harness.set_leader(True)
+    rel_id = harness.add_relation("kubeflow-profiles", "app")
+
+    harness.add_relation_unit(rel_id, "app/0")
+    data = {"service-name": "service-name", "service-port": "6666"}
+    harness.update_relation_data(
+        rel_id,
+        "app",
+        {"_supported_versions": "- v1", "data": yaml.dump(data)},
+    )
     return harness
 
 
-@patch("charm.KubernetesServicePatch", lambda x, y: None)
-def test_not_leader(harness: Harness):
-    harness.begin_with_initial_hooks()
-    assert isinstance(harness.charm.model.unit.status, WaitingStatus)
-    assert (
-        "status_set",
-        "waiting",
-        "Waiting for leadership",
-        {"is_app": False},
-    ) in harness._get_backend_calls()
+class TestCharm:
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_leader(self, harness: Harness):
+        harness.begin_with_initial_hooks()
+        assert isinstance(harness.charm.model.unit.status, WaitingStatus)
+        assert (
+            "status_set",
+            "waiting",
+            "Waiting for leadership",
+            {"is_app": False},
+        ) in harness._get_backend_calls()
 
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_model_name(self):
+        # Tests that unit will BlockStatus if deployed outside a model named kubeflow
+        # Remove when this bug is resolved: https://github.com/kubeflow/kubeflow/issues/6136
+        harness = Harness(KubeflowDashboardOperator)
+        harness.begin_with_initial_hooks()
+        assert harness.charm.model.unit.status == BlockedStatus(
+            "kubeflow-dashboard must be deployed to model named `kubeflow`:"
+            " https://git.io/J6d35"
+        )
 
-@patch("charm.KubernetesServicePatch", lambda x, y: None)
-def test_not_kubeflow_model():
-    # Tests that unit will BlockStatus if deployed outside a model named kubeflow
-    # Remove when this bug is resolved: https://github.com/kubeflow/kubeflow/issues/6136
-    harness = Harness(KubeflowDashboardOperator)
-    harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == BlockedStatus(
-        "kubeflow-dashboard must be deployed to model named `kubeflow`:"
-        " https://git.io/J6d35"
-    )
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_kf_profiles(self, harness: Harness):
+        harness.set_leader(True)
+        harness.begin_with_initial_hooks()
+
+        assert harness.charm.model.unit.status == WaitingStatus(
+            "Waiting for kubeflow-profiles relation data"
+        )
+
+    @patch("charm.KubeflowDashboardOperator._update_layer")
+    def test_on_kubeflow_dashboard_pebble_ready(
+        self, update, harness_with_relation: Harness
+    ):
+        harness_with_relation.container_pebble_ready(CHARM_NAME)
+        assert (
+            harness_with_relation.get_container_pebble_plan(
+                CHARM_NAME
+            )._services
+            is not None
+        )
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.KubeflowDashboardOperator._create_resources")
+    @patch("charm.KubeflowDashboardOperator._update_layer")
+    def test_main(self, update_layer, create_resources, harness_with_relation: Harness):
+        expected_links = BASE_SIDEBAR
+        harness_with_relation.begin_with_initial_hooks()
+        create_resources.assert_called()
+        update_layer.assert_called()
+        assert isinstance(harness_with_relation.charm.model.unit.status, ActiveStatus)
+        assert harness_with_relation.charm._context["links"] == expected_links
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.KubeflowDashboardOperator._create_resources")
+    def test_on_sidebar_relation_added(self, create_resources, harness_with_relation: Harness):
+        rel_id = harness_with_relation.add_relation("sidebar", "tensorboards-web-app")
+        harness_with_relation.add_relation_unit(rel_id, "tensorboards-web-app/0")
+        harness_with_relation.begin_with_initial_hooks()
+        create_resources.assert_called()
+        assert isinstance(harness_with_relation.charm.model.unit.status, ActiveStatus)
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.KubeflowDashboardOperator._create_resources")
+    def test_on_sidebar_relation_removed(self, create_resources, harness_with_relation: Harness):
+        rel_id = harness_with_relation.add_relation("sidebar", "tensorboards-web-app")
+        harness_with_relation.add_relation_unit(rel_id, "tensorboards-web-app/0")
+        harness_with_relation.remove_relation(rel_id)
+        harness_with_relation.begin_with_initial_hooks()
+        create_resources.assert_called()
+        assert isinstance(harness_with_relation.charm.model.unit.status, ActiveStatus)
