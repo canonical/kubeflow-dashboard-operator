4ab7fd7c47200cf230565059cb1d06fa2b5ce041
misohu <michal.hucko@canonical.com>
2022-08-04 10:26:01 +0200

    feat: add selenium integration tests between relation changes

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,22 +2,73 @@
 # See LICENSE file for licensing details.
 
 from pathlib import Path
-from subprocess import check_output
 from time import sleep
 
 import json
+from typing import Tuple
 import pytest
 import yaml
 from lightkube import Client
 from lightkube.resources.core_v1 import ConfigMap
 from selenium import webdriver
-from selenium.common.exceptions import JavascriptException, WebDriverException
+from selenium.common.exceptions import (
+    JavascriptException,
+    WebDriverException,
+    TimeoutException,
+)
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from pytest_operator.plugin import OpsTest
 
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+
+
+@pytest.fixture()
+async def driver(ops_test: OpsTest) -> Tuple[webdriver.Chrome, WebDriverWait, str]:
+    tmp = await ops_test.run(
+        "juju",
+        "status",
+        "-m",
+        ops_test.model_name,
+        "--format=yaml",
+    )
+    status = yaml.safe_load(tmp[1])
+    address = status["applications"]["kubeflow-dashboard"]["address"]
+    config = await ops_test.model.applications["kubeflow-dashboard"].get_config()
+    port = config["port"]["value"]
+    url = f"http://{address}.nip.io:{port}/"
+    options = Options()
+    options.headless = True
+
+    with webdriver.Chrome(options=options) as driver:
+        driver.delete_all_cookies()
+        wait = WebDriverWait(driver, 20, 1, (JavascriptException, StopIteration))
+        for _ in range(60):
+            try:
+                driver.get(url)
+                break
+            except WebDriverException:
+                sleep(5)
+        else:
+            driver.get(url)
+
+        yield driver, wait, url
+
+        driver.get_screenshot_as_file("/tmp/selenium-dashboard.png")
+
+
+def fix_queryselector(elems):
+    """Workaround for web components breaking querySelector.
+    Because someone thought it was a good idea to just yeet the moral equivalent
+    of iframes everywhere over a single page 🤦
+    Shadow DOM was a terrible idea and everyone involved should feel professionally
+    ashamed of themselves. Every problem it tried to solved could and should have
+    been solved in better ways that don't break the DOM.
+    """
+
+    selectors = '").shadowRoot.querySelector("'.join(elems)
+    return 'return document.querySelector("' + selectors + '")'
 
 
 @pytest.mark.abort_on_fail
@@ -62,128 +113,16 @@ async def test_status(ops_test: OpsTest):
     assert ops_test.model.applications[charm_name].units[0].workload_status == "active"
 
 
-async def test_configmap_exist(ops_test: OpsTest):
+async def test_configmap_exist():
     configmap = Client().get(ConfigMap, "centraldashboard-config")
     assert configmap is not None
 
 
-async def test_configmap_contents(ops_test: OpsTest):
-    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
-    links = json.loads(configmap.data["links"])
-    assert links == expected_links
-
-
-@pytest.mark.abort_on_fail
-async def test_add_sidebar_tensorboard_relation(ops_test: OpsTest):
-    charm_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/tensorboards-web-app_ubuntu-20.04-amd64.charm"
-    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
-    metadata = yaml.safe_load(Path(metadata_path).read_text())
-    image_path = metadata["resources"]["oci-image"]["upstream-source"]
-
-    await ops_test.model.deploy(charm_path, resources={"oci-image": image_path})
-    tensorboard_charm_name = metadata["name"]
-    charm_name = METADATA["name"]
-    await ops_test.model.add_relation(
-        f"{tensorboard_charm_name}:sidebar", f"{charm_name}:sidebar"
-    )
-    await ops_test.model.wait_for_idle(
-        [tensorboard_charm_name],
-        raise_on_blocked=True,
-        raise_on_error=True,
-        timeout=300,
-    )
-
-
-async def test_link_added_on_new_sidebar_relation(ops_test: OpsTest):
-    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
-    metadata = yaml.safe_load(Path(metadata_path).read_text())
-    tensorboard_charm_name = metadata["name"]
-    tensorboard_link = {
-        "app": tensorboard_charm_name,
-        "type": "item",
-        "link": "/tensorboards/",
-        "text": "Tensorboards",
-        "icon": "assessment",
-    }
-    base_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
-    base_links["menuLinks"] = base_links["menuLinks"] + [tensorboard_link]
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
-    links = json.loads(configmap.data["links"])
-    assert links == base_links
-
-
-async def test_link_removed_on_removed_sidebar_relation(ops_test: OpsTest):
-    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
-    metadata = yaml.safe_load(Path(metadata_path).read_text())
-    tensorboard_charm_name = metadata["name"]
-    charm_name = METADATA["name"]
-    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
-    await ops_test.run(
-        "juju",
-        "remove-relation",
-        f"{tensorboard_charm_name}:sidebar",
-        f"{charm_name}:sidebar",
-    )
-    await ops_test.model.wait_for_idle(
-        [charm_name],
-        status="active",
-        raise_on_blocked=True,
-        raise_on_error=True,
-        timeout=300,
-    )
-    configmap = Client().get(ConfigMap, "centraldashboard-config")
-    links = json.loads(configmap.data["links"])
-    assert links == expected_links
-
-
-def fix_queryselector(elems):
-    """Workaround for web components breaking querySelector.
-    Because someone thought it was a good idea to just yeet the moral equivalent
-    of iframes everywhere over a single page 🤦
-    Shadow DOM was a terrible idea and everyone involved should feel professionally
-    ashamed of themselves. Every problem it tried to solved could and should have
-    been solved in better ways that don't break the DOM.
-    """
-
-    selectors = '").shadowRoot.querySelector("'.join(elems)
-    return 'return document.querySelector("' + selectors + '")'
-
-
-@pytest.fixture()
-async def driver(request, ops_test):
-    status = yaml.safe_load(
-        check_output(
-            ["juju", "status", "-m", ops_test.model_full_name, "--format=yaml"]
-        )
-    )
-    endpoint = status["applications"]["kubeflow-dashboard"]["address"]
-    application = ops_test.model.applications["kubeflow-dashboard"]
-    config = await application.get_config()
-    port = config["port"]["value"]
-    url = f"http://{endpoint}.nip.io:{port}/"
-    options = Options()
-    options.headless = True
-
-    with webdriver.Chrome(options=options) as driver:
-        driver.delete_all_cookies()
-        wait = WebDriverWait(driver, 180, 1, (JavascriptException, StopIteration))
-        for _ in range(60):
-            try:
-                driver.get(url)
-                break
-            except WebDriverException:
-                sleep(5)
-        else:
-            driver.get(url)
-
-        yield driver, wait, url
-
-        driver.get_screenshot_as_file("/tmp/selenium-dashboard.png")
-
-
-def test_links(driver):
-    driver, wait, url = driver
+@pytest.mark.asyncio
+async def test_default_sidebar_links(
+    driver: Tuple[webdriver.Chrome, WebDriverWait, str]
+):
+    driver, wait, url = await driver.__anext__()
 
     # Ensure that sidebar links are set up properly
     links = [
@@ -235,4 +174,114 @@ def test_links(driver):
                 f"a[href='{link}']",
             ]
         )
+        wait.until(lambda x: x.execute_script(script))
+
+
+@pytest.mark.asyncio
+async def test_default_sidebar_links_missing_tensoboards(
+    driver: Tuple[webdriver.Chrome, WebDriverWait, str]
+):
+    driver, wait, url = await driver.__anext__()
+    with pytest.raises(TimeoutException):
+        script = fix_queryselector(["main-page", "iframe-link[href='/tensorboard/']"])
+        wait.until(lambda x: x.execute_script(script))
+
+
+async def test_configmap_contents(ops_test: OpsTest):
+    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
+    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    links = json.loads(configmap.data["links"])
+    assert links == expected_links
+
+
+@pytest.mark.abort_on_fail
+async def test_add_sidebar_tensorboard_relation(ops_test: OpsTest):
+    # Change to concrete charm after publish ("tensorboard")
+    charm_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/tensorboards-web-app_ubuntu-20.04-amd64.charm"
+    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
+    metadata = yaml.safe_load(Path(metadata_path).read_text())
+    image_path = metadata["resources"]["oci-image"]["upstream-source"]
+
+    await ops_test.model.deploy(charm_path, resources={"oci-image": image_path})
+    tensorboard_charm_name = metadata["name"]
+    charm_name = METADATA["name"]
+    await ops_test.model.add_relation(
+        f"{tensorboard_charm_name}:sidebar", f"{charm_name}:sidebar"
+    )
+    await ops_test.model.wait_for_idle(
+        [tensorboard_charm_name],
+        raise_on_blocked=True,
+        raise_on_error=True,
+        timeout=300,
+    )
+
+
+async def test_configmap_link_added_on_new_sidebar_relation(ops_test: OpsTest):
+    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
+    metadata = yaml.safe_load(Path(metadata_path).read_text())
+    tensorboard_charm_name = metadata["name"]
+    tensorboard_link = {
+        "app": tensorboard_charm_name,
+        "type": "item",
+        "link": "/tensorboards/",
+        "text": "Tensorboards",
+        "icon": "assessment",
+    }
+    base_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
+    base_links["menuLinks"] = base_links["menuLinks"] + [tensorboard_link]
+    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    links = json.loads(configmap.data["links"])
+    assert links == base_links
+
+
+@pytest.mark.asyncio
+async def test_tensorboard_added_sidebar_links(
+    driver: Tuple[webdriver.Chrome, WebDriverWait, str]
+):
+    driver, wait, url = await driver.__anext__()
+
+    # Ensure that sidebar links are set up properly
+    links = [
+        "/jupyter/",
+        "/pipeline/#/experiments",
+        "/pipeline/#/pipelines",
+        "/pipeline/#/runs",
+        "/pipeline/#/recurringruns",
+        "/volumes/",
+        "/tensorboards/",
+    ]
+
+    for link in links:
+        print("Looking for link: %s" % link)
+        script = fix_queryselector(["main-page", f"iframe-link[href='{link}']"])
+        wait.until(lambda x: x.execute_script(script))
+
+
+async def test_configmap_link_removed_on_removed_sidebar_relation(
+    ops_test: OpsTest, driver: Tuple[webdriver.Chrome, WebDriverWait, str]
+):
+    metadata_path = "/home/pocik/Documents/code/kubeflow-tensorboards-operator/charms/tensorboards-web-app/metadata.yaml"
+    metadata = yaml.safe_load(Path(metadata_path).read_text())
+    tensorboard_charm_name = metadata["name"]
+    charm_name = METADATA["name"]
+    expected_links = json.loads(Path("./src/config/sidebar_config.json").read_text())
+    await ops_test.run(
+        "juju",
+        "remove-relation",
+        f"{tensorboard_charm_name}:sidebar",
+        f"{charm_name}:sidebar",
+    )
+    await ops_test.model.wait_for_idle(
+        [charm_name],
+        status="active",
+        raise_on_blocked=True,
+        raise_on_error=True,
+        timeout=300,
+    )
+    configmap = Client().get(ConfigMap, "centraldashboard-config")
+    links = json.loads(configmap.data["links"])
+    assert links == expected_links
+    driver, wait, url = await driver.__anext__()
+    with pytest.raises(TimeoutException):
+        script = fix_queryselector(["main-page", "iframe-link[href='/tensorboard/']"])
         wait.until(lambda x: x.execute_script(script))
