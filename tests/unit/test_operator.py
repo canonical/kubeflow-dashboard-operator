1e9580ed787775fde72732ec3d58aa2ccb57948f
misohu <michal.hucko@canonical.com>
2022-08-09 15:59:38 +0200

    feat: add more unittests for pre pebble layer checks

--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -1,10 +1,11 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
 import yaml
 
+from lightkube.core.exceptions import ApiError
 from ops.model import BlockedStatus, WaitingStatus, ActiveStatus
 from ops.testing import Harness
 from pathlib import Path
@@ -15,6 +16,23 @@ from charm import KubeflowDashboardOperator
 BASE_SIDEBAR = Path("src/config/sidebar_config.json").read_text()
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 CHARM_NAME = METADATA["name"]
+
+
+class _FakeResponse:
+    """Used to fake an httpx response during testing only."""
+
+    def __init__(self, code):
+        self.code = code
+
+    def json(self):
+        return {"apiVersion": 1, "code": self.code, "message": "broken"}
+
+
+class _FakeApiError(ApiError):
+    """Used to simulate an ApiError during testing."""
+
+    def __init__(self, code=400):
+        super().__init__(response=_FakeResponse(code))
 
 
 @pytest.fixture(scope="function")
@@ -43,7 +61,7 @@ def harness_with_relation(harness: Harness) -> Harness:
 
 class TestCharm:
     @patch("charm.KubernetesServicePatch", lambda x, y: None)
-    def test_check_leader(self, harness: Harness):
+    def test_check_leader_failure(self, harness: Harness):
         harness.begin_with_initial_hooks()
         assert isinstance(harness.charm.model.unit.status, WaitingStatus)
         assert (
@@ -54,7 +72,19 @@ class TestCharm:
         ) in harness._get_backend_calls()
 
     @patch("charm.KubernetesServicePatch", lambda x, y: None)
-    def test_check_model_name(self):
+    def test_check_leader_success(self, harness: Harness):
+        harness.set_leader(True)
+        harness.begin_with_initial_hooks()
+        assert isinstance(harness.charm.model.unit.status, WaitingStatus)
+        assert (
+            "status_set",
+            "waiting",
+            "Waiting for leadership",
+            {"is_app": False},
+        ) not in harness._get_backend_calls()
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_model_name_failure(self):
         # Tests that unit will BlockStatus if deployed outside a model named kubeflow
         # Remove when this bug is resolved: https://github.com/kubeflow/kubeflow/issues/6136
         harness = Harness(KubeflowDashboardOperator)
@@ -65,11 +95,28 @@ class TestCharm:
         )
 
     @patch("charm.KubernetesServicePatch", lambda x, y: None)
-    def test_check_kf_profiles(self, harness: Harness):
+    def test_check_model_name_success(self, harness: Harness):
+        harness.begin_with_initial_hooks()
+        assert harness.charm.model.unit.status != BlockedStatus(
+            "kubeflow-dashboard must be deployed to model named `kubeflow`:"
+            " https://git.io/J6d35"
+        )
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_kf_profiles_failure(self, harness: Harness):
         harness.set_leader(True)
         harness.begin_with_initial_hooks()
 
         assert harness.charm.model.unit.status == WaitingStatus(
+            "Waiting for kubeflow-profiles relation data"
+        )
+
+    @patch("charm.KubeflowDashboardOperator._create_resources")
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    def test_check_kf_profiles_success(self, harness_with_relation: Harness):
+        harness_with_relation.begin_with_initial_hooks()
+
+        assert harness_with_relation.charm.model.unit.status != WaitingStatus(
             "Waiting for kubeflow-profiles relation data"
         )
 
@@ -82,6 +129,66 @@ class TestCharm:
             harness_with_relation.get_container_pebble_plan(CHARM_NAME)._services
             is not None
         )
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.Client.create")
+    def test_create_resources_success(
+        self, create: MagicMock, harness_with_relation: Harness
+    ):
+        harness_with_relation.begin_with_initial_hooks()
+        harness_with_relation.charm._context = {
+            "app_name": "kubeflow-dashboard",
+            "namespace": "kubeflow",
+            "configmap_name": "test-configmap",
+            "profilename": "test-profile",
+            "links": "",
+            "settings": "",
+        }
+        try:
+            create.assert_called()
+            harness_with_relation.charm._create_resources()
+        except Exception as e:
+            pytest.fail(e)
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.Client.create")
+    @patch("charm.Client.patch")
+    def test_create_resources_patch(
+        self, patch: MagicMock, create: MagicMock, harness_with_relation: Harness
+    ):
+        harness_with_relation.begin_with_initial_hooks()
+        harness_with_relation.charm._context = {
+            "app_name": "kubeflow-dashboard",
+            "namespace": "kubeflow",
+            "configmap_name": "test-configmap",
+            "profilename": "test-profile",
+            "links": "",
+            "settings": "",
+        }
+        create.side_effect = _FakeApiError(code=409)
+        harness_with_relation.charm._create_resources()
+        patch.assert_called()
+
+    @patch("charm.KubernetesServicePatch", lambda x, y: None)
+    @patch("charm.Client.create")
+    @patch("charm.Client.patch")
+    def test_create_resources_failure(
+        self, patch: MagicMock, create: MagicMock, harness_with_relation: Harness
+    ):
+        harness_with_relation.begin_with_initial_hooks()
+        harness_with_relation.charm._context = {
+            "app_name": "kubeflow-dashboard",
+            "namespace": "kubeflow",
+            "configmap_name": "test-configmap",
+            "profilename": "test-profile",
+            "links": "",
+            "settings": "",
+        }
+        create.side_effect = _FakeApiError(code=409)
+        patch.side_effect = _FakeApiError(code=410)
+        # any other error then 409 and 410 will fail the test
+        with pytest.raises(ApiError):
+            harness_with_relation.charm._create_resources()
 
     @patch("charm.KubernetesServicePatch", lambda x, y: None)
     @patch("charm.KubeflowDashboardOperator._create_resources")
