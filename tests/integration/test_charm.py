4dbac743578d768694c90a3bf0a15cc7a026a665
Ivan Chvets <ivan.chvets@canonical.com>
2023-03-17 16:16:45 -0400

    fix: updated integration test https://github.com/canonical/kubeflow-dashboard-operator/issues/26
    Summary of changes:
    - Updated doc link in integration test.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -159,7 +159,7 @@ def test_default_sidebar_links(driver: Tuple[webdriver.Chrome, WebDriverWait, st
 
     # Ensure that doc links are set up properly
     links = [
-        "https://charmed-kubeflow.io/docs/get-started-with-charmed-kubeflow",
+        "https://charmed-kubeflow.io/docs/get-started-with-charmed-kubeflow#heading--part-ii-get-started-with-charmed-kubeflow"
         "https://charmed-kubeflow.io/docs/get-started-with-charmed-kubeflow#heading--install-and-prepare-microk8s-",  # noqa: E501
         "https://www.kubeflow.org/docs/started/requirements/",
     ]
