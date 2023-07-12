a170abb40be6cffb904f83d1cca6f9dd61727512
Andrew Scribner <ca.scribner+1@gmail.com>
2023-07-12 16:01:14 -0400

    bump lib version

--- lib/charms/kubeflow_dashboard/v0/kubeflow_dashboard_links.py
+++ lib/charms/kubeflow_dashboard/v0/kubeflow_dashboard_links.py
@@ -75,7 +75,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 
 DASHBOARD_LINK_LOCATIONS = ['menu', 'external', 'quick', 'documentation']
