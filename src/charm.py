af9ea4766d5c2be3d6730a16a0e9e5be6560854a
mvlassis <manos.vlassis@canonical.com>
2024-07-24 11:51:07 +0300

    Fix linting

--- src/charm.py
+++ src/charm.py
@@ -181,7 +181,7 @@ class KubeflowDashboardOperator(CharmBase):
                         "DASHBOARD_CONFIGMAP": self._configmap_name,
                         "LOGOUT_URL": "/authservice/logout",
                         "POD_NAMESPACE": self.model.name,  # Added due to https://github.com/canonical/bundle-kubeflow/issues/698  # noqa E501
-                        "NODE_ENV": "production"
+                        "NODE_ENV": "production",
                     },
                 }
             },
