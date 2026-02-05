88c9bee578a6197468ff28d75aa9445539610436
NohaIhab <noha.ihab@canonical.com>
2026-02-05 18:48:38 +0200

    chore: update terraform outputs to include ambient mesh relations

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,16 +4,20 @@ output "app_name" {
 
 output "provides" {
   value = {
-    links             = "links",
     grafana_dashboard = "grafana-dashboard",
-    metrics_endpoint  = "metrics-endpoint"
+    links             = "links",
+    metrics_endpoint  = "metrics-endpoint",
+    provide_cmr_mesh  = "provide-cmr-mesh"
   }
 }
 
 output "requires" {
   value = {
-    ingress           = "ingress",
-    kubeflow_profiles = "kubeflow-profiles",
-    logging           = "logging"
+    ingress             = "ingress",
+    istio_ingress_route = "istio-ingress-route",
+    kubeflow_profiles   = "kubeflow-profiles",
+    logging             = "logging",
+    require_cmr_mesh    = "require-cmr-mesh",
+    service_mesh        = "service-mesh"
   }
 }
